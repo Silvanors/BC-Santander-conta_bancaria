b650e5043a2a1f2a16eb9002d70c8c961b31e919
Silvanors <silvanors@gmail.com>
2023-09-25 16:57:03 -0300

    Entrega do desafio

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# BC-Santander-conta_bancaria
-Java básico - simulação de conta bancária
+# DIO - Trilha Java Básico
+www.dio.me
+
+#### Autor
+- Alunos [Silvano Rodrigues](https://github.com/silvanors)
+
+## Sintaxe - Desafio
+
+1. Este é um projeto-desafio de java básico onde criamos uma classe `ContaTerminal.java`, na qual o usuário insere o nome, o código da agência e número da conta, e em seguida é apresentado as informações inseridas juntamente com o saldo disponível na conta com a mensagem *"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".*
+
+2. Foram declaradas as variáveis:
+* String nomeCliente;
+* String agencia;
+* Integer numeroDaConta;
+* Double saldo;
+
+3. Foi utilizado a classe Scanner para obter os dados via terminal.
+
+4. Neste projeto não fizemos tratamento de Exceptions.
+
+
+###### Exemplo apresentado:
+
+| Atributo  | Tipo     | Exemplo   
+| --------- | ---------| ------- 
+| Numero    | Inteiro  | 1021 
+| Agencia   | Texto    | 067-8
+| Nome Cliente | Texto    | MARIO ANDRADE
+| Saldo | Decimal |237.48
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    
+   public static void main(String[] args) throws Exception {
         
         String nomeCliente;
         String agencia;
         Integer numeroDaConta;
-        Double saldo;
-
+        Double saldo = 49.75;
+        
         //TODO: conhecer e importar a classe Scanner
         //Obter pela scanner os valores digitados no terminal
                 
        
         Scanner leitor = new Scanner(System.in);   
-            
+         
         
-        //Exibir as mensagens para o nosso usuário
-        System.out.println("Por favor, digite seu nome completo, em seguida tecle 'Enter'!");
-           nomeCliente = leitor.nextLine();
+         //Exibir as mensagens para o nosso usuário
+         System.out.println("Por favor, digite seu nome completo, em seguida tecle 'Enter'!");
+            nomeCliente = leitor.nextLine().toUpperCase();
 
-        System.out.println("Por favor, informe sua agência, em seguida tecle 'Enter'!");
-           agencia = leitor.nextLine();
+         System.out.println("Por favor, informe sua agência, em seguida tecle 'Enter'!");
+            agencia = leitor.nextLine().toUpperCase();
 
-        System.out.println("Por favor, digite o número de sua conta, em seguida tecle 'Enter'!");
-           numeroDaConta = leitor.nextInt();
-        
-        System.out.println("Por favor, digite seu saldo, em seguida tecle 'Enter'!");
-           saldo = leitor.nextDouble();
-        
-        //Exibir a mensagem conta criada
+         
+         System.out.println("Por favor, digite o número de sua conta, em seguida tecle 'Enter'!");
+            numeroDaConta = leitor.nextInt();
+
+         
+               //Exibir a mensagem conta criada
         System.out.println("*Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco!"+
-        "\n"+" Sua agência é "+ agencia+", conta "+numeroDaConta+" e seu saldo "+ String.format("%.2f", saldo) +" já está disponível para saque.*");
-
-    }
+        "\n"+" Sua agência é "+ agencia+", conta "+numeroDaConta+" e seu saldo R$"+ String.format("%.2f", saldo) +" já está disponível para saque.*");
+            
+         }
 }
